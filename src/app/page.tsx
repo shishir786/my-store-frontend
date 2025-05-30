df88492b076c38c1d3be7cfcc4cd0ed7b3651c4a
Abdullah Shishir <abdullahshishir786@gmail.com>
2025-05-30 16:53:21 +0600

    dep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-
-interface ApiError {
-  message?: string;
-  response?: {
-    data?: unknown;
-  };
-}
 
 const Home = () => {
   const [domain, setDomain] = useState('');
